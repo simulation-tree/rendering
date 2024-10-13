a515de52206dfae3b245a705cf0952163c95c34d
Phill <philldamaskin@gmail.com>
2024-10-13 12:52:00 -0400

    polish render function and catching up with span changes

--- source/Render System/Functions/RenderFunction.cs
+++ source/Render System/Functions/RenderFunction.cs
@@ -8,16 +8,16 @@ namespace Rendering.Functions
     public unsafe readonly struct RenderFunction
     {
 #if NET
-        private readonly delegate* unmanaged<Allocation, uint*, uint, uint, uint, uint, void> function;
+        private readonly delegate* unmanaged<Input, void> function;
 
-        public RenderFunction(delegate* unmanaged<Allocation, uint*, uint, uint, uint, uint, void> function)
+        public RenderFunction(delegate* unmanaged<Input, void> function)
         {
             this.function = function;
         }
 #else
-        private readonly delegate*<Allocation, uint*, uint, uint, uint, uint, void> function;
+        private readonly delegate*<Input, void> function;
 
-        public RenderFunction(delegate*<Allocation, uint*, uint, uint, uint, uint, void> function)
+        public RenderFunction(delegate*<Input, void> function)
         {
             this.function = function;
         }
@@ -25,7 +25,33 @@ namespace Rendering.Functions
 
         public readonly void Invoke(Allocation system, USpan<uint> renderers, uint material, uint shader, uint mesh)
         {
-            function(system, renderers.pointer, renderers.Length, material, shader, mesh);
+            function(new(system, renderers, material, shader, mesh));
+        }
+
+        public readonly struct Input
+        {
+            public readonly Allocation system;
+            public readonly uint material;
+            public readonly uint shader;
+            public readonly uint mesh;
+
+            private readonly nint address;
+            private readonly uint length;
+
+            /// <summary>
+            /// All entities containing the same filter, callback and identifier combinations.
+            /// </summary>
+            public readonly USpan<uint> Renderers => new(address, length);
+
+            public Input(Allocation system, USpan<uint> renderers, uint material, uint shader, uint mesh)
+            {
+                this.system = system;
+                this.material = material;
+                this.shader = shader;
+                this.mesh = mesh;
+                address = renderers.Address;
+                length = renderers.Length;
+            }
         }
     }
 }
