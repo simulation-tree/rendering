62e6ee0faed92e8605b0d81801c52e1d15cfb1c1
Phill <philldamaskin@gmail.com>
2025-01-24 22:17:00 -0500

    reference generators and simplify tests initialization

--- tests/RenderingTests.cs
+++ tests/RenderingTests.cs
@@ -1,10 +1,6 @@
-﻿using Data.Components;
-using Meshes;
-using Meshes.Components;
-using Rendering.Components;
-using Shaders.Components;
-using Simulation.Tests;
+﻿using Simulation.Tests;
 using Types;
+using Worlds;
 
 namespace Rendering.Tests
 {
@@ -12,45 +8,20 @@ namespace Rendering.Tests
     {
         static RenderingTests()
         {
-            TypeLayout.Register<IsDestination>();
-            TypeLayout.Register<IsMaterial>();
-            TypeLayout.Register<IsDataRequest>();
-            TypeLayout.Register<RendererScissor>();
-            TypeLayout.Register<WorldRendererScissor>();
-            TypeLayout.Register<RendererInstanceInUse>();
-            TypeLayout.Register<SurfaceInUse>();
-            TypeLayout.Register<IsViewport>();
-            TypeLayout.Register<IsRenderer>();
-            TypeLayout.Register<IsMesh>();
-            TypeLayout.Register<IsShader>();
-            TypeLayout.Register<IsShaderRequest>();
-            TypeLayout.Register<MeshVertexIndex>();
-            TypeLayout.Register<MaterialPushBinding>();
-            TypeLayout.Register<MaterialComponentBinding>();
-            TypeLayout.Register<MaterialTextureBinding>();
-            TypeLayout.Register<DestinationExtension>();
+            TypeRegistry.Load<Data.TypeBank>();
+            TypeRegistry.Load<Meshes.TypeBank>();
+            TypeRegistry.Load<Rendering.TypeBank>();
+            TypeRegistry.Load<Shaders.TypeBank>();
         }
 
-        protected override void SetUp()
+        protected override Schema CreateSchema()
         {
-            base.SetUp();
-            world.Schema.RegisterComponent<IsDestination>();
-            world.Schema.RegisterComponent<IsMaterial>();
-            world.Schema.RegisterComponent<IsDataRequest>();
-            world.Schema.RegisterComponent<RendererScissor>();
-            world.Schema.RegisterComponent<WorldRendererScissor>();
-            world.Schema.RegisterComponent<RendererInstanceInUse>();
-            world.Schema.RegisterComponent<SurfaceInUse>();
-            world.Schema.RegisterComponent<IsViewport>();
-            world.Schema.RegisterComponent<IsRenderer>();
-            world.Schema.RegisterComponent<IsMesh>();
-            world.Schema.RegisterComponent<IsShader>();
-            world.Schema.RegisterComponent<IsShaderRequest>();
-            world.Schema.RegisterArrayElement<MeshVertexIndex>();
-            world.Schema.RegisterArrayElement<MaterialPushBinding>();
-            world.Schema.RegisterArrayElement<MaterialComponentBinding>();
-            world.Schema.RegisterArrayElement<MaterialTextureBinding>();
-            world.Schema.RegisterArrayElement<DestinationExtension>();
+            Schema schema = base.CreateSchema();
+            schema.Load<Data.SchemaBank>();
+            schema.Load<Meshes.SchemaBank>();
+            schema.Load<Rendering.SchemaBank>();
+            schema.Load<Shaders.SchemaBank>();
+            return schema;
         }
     }
 }
